c767a0aecfa76712a5793a85845d8661051e6680
Fares Elsobky <fares@Faress-MacBook-Air.local>
2026-07-26 22:23:48 +0300

    feat: initialize Angular portfolio

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from './shared/components/footer/footer';
+
+import { Home } from './features/home/home';
+import { About } from './features/about/about';
+import { Experience } from './features/experience/experience';
+import { Education } from './features/education/education';
+import { Projects } from './features/projects/projects';
+import { Skills } from './features/skills/skills';
+import { Contact } from './features/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    Home,
+    About,
+    Experience,
+    Education,
+    Projects,
+    Skills,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('portfolio');
-}
+export class App {}
---
+++ portfolio/src/app/shared/components/section-title/section-title.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-title',
+  imports: [],
+  templateUrl: './section-title.html',
+  styleUrl: './section-title.scss',
+})
+export class SectionTitle {}
